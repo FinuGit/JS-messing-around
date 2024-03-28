2adc42cb55dbd84cf89d382b2fb2566527fb30ed
FinuGit <finuarts@gmail.com>
2024-03-28 15:18:36 +0100

    added 1-3 positions on page

--- myScript.js
+++ myScript.js
@@ -1,3 +1,10 @@
-function myFunction(){
-    document.getElementById("demo").innerHTML = "Testing the change of html via JS"
+function firstTest(){
+    document.getElementById("test1").innerHTML = "Testing the change of html via JS";
+}
+function alertMathAddition(){
+    let x = 5, y = 6
+    alert(x + y);
+}
+function time(){
+    document.getElementById("time").innerHTML=Date();
 }
