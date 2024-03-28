ea4c838e96f12c1d9c4d65808e325505ea43b4c3
FinuGit <finuarts@gmail.com>
2024-03-28 20:03:40 +0100

    console tests, started counting position

--- myScript.js
+++ myScript.js
@@ -13,3 +13,28 @@ function charAt(){
     let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     document.getElementById("getChar").innerHTML = text.charAt(x);
 }
+function shout(){
+    let text = document.getElementById("hello").innerHTML;
+    document.getElementById("hello").innerHTML = text.toLocaleUpperCase();
+}
+
+/*let count = 0;
+
+console.log(count);
+
+let myAge = 24;
+console.log(myAge);
+
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);*/
+
+let countEl = document.getElementById("countClicks");
+console.log(countEl);
+
+let count = 0
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+}
